ff9781f17a20a42e891676b58806e1b0e2ac4d9a
John Smith <johnsmithcoder1@gmail.com>
2021-08-16 14:54:04 -0500

    Add route to persist uploaded images in fileServer

--- Sources/Application/Routes/ImageRoutes.swift
+++ Sources/Application/Routes/ImageRoutes.swift
@@ -11,6 +11,37 @@ import Kitura
 func initializeImageRoutes(app: App) throws {
     let fileServer = try setupFileServer()
     app.router.get("/api/v1/images", middleware: fileServer)
+    app.router.post("/api/v1/image") { request, response, next in
+        defer { next() }
+        
+        guard let filename = request.headers["Slug"] else {
+            response.status(.preconditionFailed).send("Filename not specified")
+            
+            return
+        }
+        
+        var imageData = Data()
+        
+        do {
+            try _ = request.read(into: &imageData)
+        } catch let readError {
+            response.status(.internalServerError).send("Unable to read image data: \(readError.localizedDescription)")
+            
+            return
+        }
+        
+        do {
+            let fullPath = "\(fileServer.absoluteRootPath)/\(filename)"
+            let fileUrl = URL(fileURLWithPath: fullPath)
+            
+            try imageData.write(to: fileUrl)
+            response.status(.created).send("Image created")
+        } catch let writeError {
+            response.status(.internalServerError).send("Unable to write image data: \(writeError.localizedDescription)")
+            
+            return
+        }
+    }
 }
 
 private func setupFileServer() throws -> StaticFileServer {
