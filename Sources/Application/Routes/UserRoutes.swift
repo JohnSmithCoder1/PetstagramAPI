96c55af36a4191d9e8b0bcbc3d5f903ec7f308af
John Smith <johnsmithcoder1@gmail.com>
2021-08-16 11:59:12 -0500

    Add getUser to UserRoutes

--- Sources/Application/Routes/UserRoutes.swift
+++ Sources/Application/Routes/UserRoutes.swift
@@ -15,6 +15,16 @@ func initializeUserRoutes(app: App) {
     app.router.post("/api/v1/user", handler: addUser)
 }
 
+func getUser(query: UserParams, completion: @escaping (UserAuthentication?, RequestError?) -> Void) {
+    guard let foundUser = users.first(where: { $0.id == query.id }), foundUser.password == query.password else {
+        completion(nil, RequestError.unauthorized)
+        
+        return
+    }
+    
+    completion(foundUser, nil)
+}
+
 func addUser(user: UserAuthentication, completion: @escaping (UserAuthentication?, RequestError?) -> Void) {
     users.append(user)
     completion(user, nil)
